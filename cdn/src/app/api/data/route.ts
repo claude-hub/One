6b82ef4a8343dac9dc73b203258342e511272daa
claude-hub <claudez1115@gmail.com>
2025-03-01 23:10:59 +0800

    feat: 🎸 change data

--- cdn/src/app/api/data/route.ts
+++ cdn/src/app/api/data/route.ts
@@ -2,19 +2,12 @@ import data from '@/services/data.json';
 
 export async function GET() {
   // 设置响应头允许跨域访问
-  return new Response(
-    JSON.stringify({
-      code: 200,
-      data,
-      msg: 'success'
-    }),
-    {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        'Content-Type': 'application/json'
-      }
+  return new Response(JSON.stringify(data), {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Content-Type': 'application/json'
     }
-  );
+  });
 }
